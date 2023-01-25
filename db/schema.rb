ff52079db85d0e2e8ac33c10e5392631a7c0eca1
Will Richards <spectralsounddesign@gmail.com>
2023-01-25 14:00:19 -0500

    Edited production.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_045001) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_080313) do
   create_table "entries", force: :cascade do |t|
     t.string "song_title"
     t.string "year"
@@ -37,5 +21,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_045001) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "articles"
 end
